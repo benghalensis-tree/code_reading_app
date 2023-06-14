ea1fbd83dd0f4bf5cf821be7f98e3ba149e8a36a
genki utsumi <zabanya0770@gmail.com>
2023-06-14 16:00:44 +0900

    コントローラで削除できないように修正

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -16,9 +16,13 @@ class AssignsController < ApplicationController
 
   def destroy
     assign = Assign.find(params[:id])
-    destroy_message = assign_destroy(assign, assign.user)
+    if current_user.id == assign.team.owner_id
+      destroy_message = assign_destroy(assign, assign.user)
 
-    redirect_to team_url(params[:team_id]), notice: destroy_message
+      redirect_to team_url(params[:team_id]), notice: destroy_message
+    else
+      redirect_to team_url(params[:team_id]), notice: '削除できません'
+    end
   end
 
   private
