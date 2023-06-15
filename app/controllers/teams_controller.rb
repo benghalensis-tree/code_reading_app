7a29c68132d932e6d051e488c3c2836263f167bb
genki utsumi <zabanya0770@gmail.com>
2023-06-15 13:41:07 +0900

    リーダーのみ権限の移動ができるように修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -39,13 +39,18 @@ class TeamsController < ApplicationController
   end
 
   def assign_owner
-    if @team.update(owner_id: params[:owner_id])
-      ContactMailer.contact_mail(@team).deliver 
-      @user = User.find(@team.owner_id)
-      redirect_to team_path, notice: 'オーナー権限が移動しました!'
+    if current_user.id == @team.owner_id
+      if @team.update(owner_id: params[:owner_id])
+        ContactMailer.contact_mail(@team).deliver 
+        @user = User.find(@team.owner_id)
+        redirect_to team_path, notice: 'オーナー権限が移動しました!'
+      else
+        redirect_to team_path, notice: '権限の移動に失敗しました'
+      end
     else
       redirect_to team_path, notice: '権限の移動に失敗しました'
     end
+
   end
 
   def destroy
